2c990cfacb688b23574b52c6d2f9c2a07a7c2241
blendedmarks <blendedmarks@gmail.com>
2017-02-10 18:22:51 +0000

    Update BlenderInstall.sh

--- BlenderInstall.sh
+++ BlenderInstall.sh
@@ -26,7 +26,7 @@ if (uname -m | grep '64'); then
   echo ""
   echo "Downloading 64-bit Blender"
   echo ""
-  wget http://download.blender.org/release/Blender2.78/blender-2.78a-linux-glibc219-x86_64.tar.bz2
+  wget http://download.blender.org/release/Blender2.78/blender-2.78b-linux-glibc219-x86_64.tar.bz2
 #######################################################################
 
 ##Create Directory For Blender To Be Installed To
@@ -68,7 +68,7 @@ tar xvjf blender-2.78a-linux-glibc219-x86_64.tar.bz2
 else
   echo "ARCH: 32-bit"
   echo "Downloading 32-bit Blender"
-  wget http://download.blender.org/release/Blender2.78/blender-2.78a-linux-glibc219-i686.tar.bz2
+  wget http://download.blender.org/release/Blender2.78/blender-2.78b-linux-glibc219-i686.tar.bz2
 
   ##Create Directory For Blender To Be Installed To
 echo ""
